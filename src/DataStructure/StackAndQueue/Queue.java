1eee13839831d13b2565bbe115423d11d64644c6
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2024-07-28 23:13:47 +0900

    Stack&Queue 1일차

---
+++ src/DataStructure/StackAndQueue/Queue.java
@@ -0,0 +1,43 @@
+package DataStructure.StackAndQueue;
+
+import java.util.NoSuchElementException;
+
+// 큐를 구현하시오.
+public class Queue<T> {
+    private Node<T> first;
+    private Node<T> last;
+
+    public void add(T item) {
+        Node<T> t = new Node<T>(item);
+        if(last != null) {
+            last.next = t;
+        }
+        last = t;
+        if(first == null) {
+            first = last;
+        }
+    }
+
+    public T remove() {
+        if(first == null) {
+            throw new NoSuchElementException();
+        }
+        T item = first.data;
+        first = first.next;
+        if(first == null) {
+            last = null;
+        }
+        return item;
+    }
+
+    public T peek() {
+        if(first == null) {
+            throw new NoSuchElementException();
+        }
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+}
---
+++ src/DataStructure/StackAndQueue/Stack.java
@@ -0,0 +1,34 @@
+package DataStructure.StackAndQueue;
+
+import java.util.EmptyStackException;
+
+// 스택을 구현하시오.
+public class Stack<T> {
+    private Node<T> top;
+
+    public T pop() {
+        if(top == null) {
+            throw new EmptyStackException();
+        }
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    public void push(T item) {
+        Node<T> t = new Node<T>(item);
+        t.next = top;
+        top = t;
+    }
+
+    public T peek() {
+        if(top == null) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+}
