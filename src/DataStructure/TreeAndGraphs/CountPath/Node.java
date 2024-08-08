4459c820b6708513f108ca1c841c64586e9328b8
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2024-08-08 22:35:01 +0900

    Tree&Graph 8일차

---
+++ src/DataStructure/TreeAndGraphs/CountPath/Node.java
@@ -0,0 +1,11 @@
+package DataStructure.TreeAndGraphs.CountPath;
+
+public class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
