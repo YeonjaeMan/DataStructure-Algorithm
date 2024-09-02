769f0f182d4c134e3c91efa3855107810efcb7d1
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2024-09-03 00:07:45 +0900

    프로그래머스 고득점 알고리즘 Kit 완전탐색 4번 문제

--- src/CodingTest/ExhaustiveSearch/카펫.java
+++ src/CodingTest/ExhaustiveSearch/카펫.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 // 프로그래머스 고득점 알고리즘 Kit 완전탐색 4번 문제
+/*
+1. yellow의 약수 쌍을 list에 저장.
+2. (yellow의 가로 + 2) * (yellow의 세로 + 2) - yellow = brown 임을 사용.
+ */
 public class 카펫 {
     private List<int[]> list;
 
@@ -11,7 +15,7 @@ public class 카펫 {
         int[] answer = new int[2];
         findNum(yellow);
         for(int[] arr : list) {
-            if((arr[0] + 2) * (arr[1] + 2) - brown == yellow) {
+            if((arr[0] + 2) * (arr[1] + 2) - yellow == brown) {
                 answer[0] = arr[0] + 2;
                 answer[1] = arr[1] + 2;
                 break;
@@ -20,16 +24,13 @@ public class 카펫 {
         return answer;
     }
 
-    // 시간 초과..
     private void findNum(int yellow) {
         list = new ArrayList<>();
         for(int i = yellow; i >= 1; i--) {
-            for(int j = 1; j <= yellow / 2 + 1; j++) {
-                if(i * j == yellow) {
-                    int[] tmp = {i, j};
-                    list.add(tmp);
-                    break;
-                }
+            int j = yellow / i;
+            if(i * j == yellow) {
+                int[] tmp = {Math.max(i, j), Math.min(i, j)};
+                list.add(tmp);
             }
         }
     }
